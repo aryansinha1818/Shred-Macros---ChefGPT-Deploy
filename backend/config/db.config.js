1f145caab5a0022b93cd87be385078b6a4f82543
Aryan Sinha <aryansinha18@Aryans-MacBook-Air.local>
2025-12-28 00:49:03 +0530

    uri

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   await mongoose
-    .connect(process.env.MONGODB_URL)
+    .connect(process.env.MONGODB_URI)
     .then(() => console.log("Connected to DB"));
 };
 module.exports = connectDB;
