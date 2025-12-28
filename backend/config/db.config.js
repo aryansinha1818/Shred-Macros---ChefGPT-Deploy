8ede12aed88db60994c56117e259aa1cb97e725d
Aryan Sinha <aryansinha18@Aryans-MacBook-Air.local>
2025-12-28 15:14:55 +0530

    http6

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,9 +1,13 @@
+const mongoose = require("mongoose");
+
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
-    console.log("Connected to DB");
-  } catch (err) {
-    console.error("Mongo connection failed", err.message);
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error(error);
     process.exit(1);
   }
 };
+
+module.exports = connectDB;
